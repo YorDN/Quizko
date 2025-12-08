c1e41ba9f368f61e13be658f13d2d5fac0573537
Yordan <mixdata34@gmail.com>
2025-12-08 08:35:37 +0200

    Added props in Subject.cs

--- Quizko.Data.Models/Quizes/Subject.cs
+++ Quizko.Data.Models/Quizes/Subject.cs
@@ -1,6 +1,15 @@
-﻿namespace Quizko.Data.Models.Quizes
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Quizko.Data.Models.Quizes
 {
     public class Subject
     {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required] 
+        public string Name { get; set; } = null!;
+
+        public ICollection<Question> Questions { get; set; } = new List<Question>();
     }
 }
